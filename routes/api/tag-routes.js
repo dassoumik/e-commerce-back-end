d4693988647c46e2eca8eaeabade38bebadd3945
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-18 05:33:52 -0400

    added logic for get and post for api/tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    
+    const tagAllData  = await Tag.findAll({
+      include:[{model: Product, as: 'productTags'}],
+    });
+    res.status(200).json(tagAllData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagAllData  = await Tag.findByPk(req.params.id, {
+      include:[{model: Product, as: 'productTags'}],
+    });
+    res.status(200).json(tagAllData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
