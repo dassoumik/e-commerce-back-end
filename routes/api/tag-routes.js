5935514bd74b1a7a41eab321e0c9ec1ec20051c9
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-18 05:37:58 -0400

    added logic for put - api/tags route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,8 +41,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagUpdateData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    }).then(tagUpdateData => {
+    if (!tagUpdateData[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagUpdateData);});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
